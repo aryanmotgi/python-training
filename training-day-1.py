c8a7ef75cd6a914fedcd396bdd9a29b161c1e767
Aryan Motgi <aryanmotgi@Aryans-MBP.localdomain>
2021-06-18 10:07:04 -0700

    Initial verision

---
+++ training-day-1.py
@@ -0,0 +1,26 @@
+print("hello world!")
+#adding 5 to 5
+print(5+5) #Have to use print for the first line if your using two lines of code in math.
+type(3.0)
+#PEMDAS
+
+print(52+4**10-2)
+
+import math #pi
+
+print(math.cos(math.pi / 2))
+
+import math #square root √
+
+print(math.sqrt(144))
+
+import random
+
+random.randrange(1,20)
+
+print("\"Hello world\"")
+
+print(2+3)
+print("2"+"3")
+print("ARY" "AN")
+3*(2**4)/12
---
+++ training-day-2.py
@@ -0,0 +1,94 @@
+#varibles
+name="dog"
+
+print(name)
+
+
+
+# random die rolling
+import random,
+
+die_roll_4 = random.randint(1,4)
+die_roll_6 = random.randint(1,6)
+die_roll_10 = random.randint(1,10)
+
+print("the 4-sided die rolled a ",die_roll_4)
+print("the 6-sided die rolled a ",die_roll_6)
+print("the 10-sided die rolled a ", die_roll_10)
+
+
+
+#varibles
+num1=1
+num2=2
+greeting1="hello"
+greeting2="hello"
+
+print(num1)
+print(num2)
+print(greeting1)
+print(greeting2)
+
+
+
+#varible adding
+num1=5
+num2=6
+
+print(num1)
+print(num2)
+print(num1+num2)
+
+#practive for die random die rolling
+import random
+
+die_roll_100 = random.randint(1,100)
+
+print("the 100-sided die rolled a",die_roll_100)
+
+
+
+#math and varibles
+x=1
+x-=10
+print(x)
+
+x=25
+x/=5
+print(x)
+
+x=23
+x%=3
+print(x)
+
+
+import math
+x=20
+x=math.cos(x)
+print(x)
+
+print(type(11))
+
+numl=15
+print(type(num1))
+
+num2-15.0
+print(type(num2))
+
+msg = "this is a messge"
+print(type(msg))
+
+x = 5
+print(x)
+
+x = "abc"
+print(x)
+
+
+
+a = 7
+print(a*2)
+print(a+4)
+print(a/3)
+print(a*6)
+print(a*2+4/3*6)
